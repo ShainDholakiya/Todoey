11a576c65f98fa227d2640eba7d5a7e94c288c89
Shain Dholakiya <shainshain99@gmail.com>
2020-05-30 20:51:13 -0400

    updated files

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Shain Dholakiya on 5/29/20.
+//  Copyright © 2020 Shain Dholakiya. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Shain Dholakiya on 5/29/20.
+//  Copyright © 2020 Shain Dholakiya. All rights reserved.
+//
+
+import Foundation
